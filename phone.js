94fde3a5fb072f9d0c821459bb8b985ec6604a60
RiyadMia <96831477+RiyadMia@users.noreply.github.com>
2022-03-01 21:25:15 +0600

    last fill added

--- phone.js
+++ phone.js
@@ -13,26 +13,23 @@ const searchPhone = () => {
         `
         fetch(url)
             .then(res => res.json())
-            .then(phones => displayPhoneResult(phones.data))
+            .then(data => displayPhoneResult(data.data))
     }
 }
-
 const displayPhoneResult = data => {
     const searchResult = document.getElementById('search-result')
-
     searchResult.textContent = '';
     notFound.style.display = 'none'
-    data.forEach(phone => {
+    const sliceResult = data.slice(0, 20)
+    sliceResult.forEach(phone => {
         const div = document.createElement('div')
-
         div.classList.add('col');
-
         div.innerHTML = `
     <div onclick="lodePhoneDetails('${phone.slug}')" class="card h-100">
-    <h1>${phone.brand}</h1>
     <img src="${phone.image}" class="card-img-top" alt="...">
     <div class="card-body">
     <h1 class="card-title">${phone.phone_name}</h1>
+    <h3>${phone.brand}</h3>
     </div>
     </div>
     `
@@ -55,15 +52,14 @@ const displayPhoneDetail = phone => {
     phoneDatails.textContent = '';
     const div = document.createElement('div')
     div.classList.add('col');
-
     div.innerHTML = `  
     <img src="${phone.image}" class="card-img-top" alt="...">
     <div class="card-body">
     <h1 class="card-title">${phone.name}</h1>
     </div>
     <button onclick="buttonDetails('${phone.slug}')"> Details </button>
-    <button onclick="sensorDetails('${phone.slug}')"> Sensor </button>
     <button onclick="buttonOthers('${phone.slug}')"> Others </button> 
+    <button onclick="relesButton('${phone.slug}')"> Releas </button> 
     </div>
     `
     phoneDatails.appendChild(div);
@@ -85,38 +81,17 @@ const displayButtonDetails = phone => {
     div.innerHTML = `  
     <div class="card-body">
     <h1> MainFeatures </h1>
-    <h1> storage : ${phone.storage}</h1>
-    <h1> chipSet : ${phone.chipSet}</h1>
-    <h1> displaySize : ${phone.displaySize}</h1>
-    <h1>  memory : ${phone.memory}</h1>
-    </div>
-    `
-    phoneDatails.appendChild(div);
-}
-
-// sensor
-const sensorDetails = slug => {
-    const url = `
-    https://openapi.programming-hero.com/api/phone/${slug}
-    `
-    fetch(url)
-        .then(res => res.json())
-        // .then(phones => console.log(phones.data.mainFeatures.sensors[0]))
-        .then(phones => displaySensorDetails(phones.data.mainFeatures))
-}
-const displaySensorDetails = phone => {
-    const phoneDatails = document.getElementById('phone-details')
-    const div = document.createElement('div')
-    div.classList.add('col');
-    div.innerHTML = `  
-    <div class="card-body">
+    <h4> storage : ${phone.storage}</h4>
+    <h4> chipSet : ${phone.chipSet}</h4>
+    <h4> displaySize : ${phone.displaySize}</h4>
+    <h4>  memory : ${phone.memory}</h4>
     <h1> Sensors  </h1>
-    <h1>1. ${phone.sensors[0]} </h1>
-    <h1>2. ${phone.sensors[1]} </h1>
-    <h1>3. ${phone.sensors[2]} </h1>
-    <h1>4. ${phone.sensors[3]} </h1>
-    <h1>5. ${phone.sensors[4]} </h1>
-    <h1>6. ${phone.sensors[5]} </h1>
+    <h4>1. ${phone.sensors[0]} </h4>
+    <h4>2. ${phone.sensors[1]} </h4>
+    <h4>3. ${phone.sensors[2]} </h4>
+    <h4>4. ${phone.sensors[3]} </h4>
+    <h4>5. ${phone.sensors[4]} </h4>
+    <h4>6. ${phone.sensors[5]} </h4>
     </div>
     `
     phoneDatails.appendChild(div);
@@ -140,14 +115,36 @@ const displayOtherDetails = phone => {
     div.innerHTML = `  
     <div class="card-body">
     <h1> Others  </h1>
-    <h1> Bluetooth : ${phone.Bluetooth}</h1>
-    <h1>  GPS : ${phone.GPS}</h1>
-    <h1>  NFC : ${phone.NFC}</h1>
-    <h1>  Radio : ${phone.Radio}</h1>
-    <h1>  USB : ${phone.USB}</h1>
-    <h1> WLAN  : ${phone.WLAN}</h1>
+    <h4> Bluetooth : ${phone.Bluetooth}</h4>
+    <h4>  GPS : ${phone.GPS}</h4>
+    <h4>  NFC : ${phone.NFC}</h4>
+    <h4>  Radio : ${phone.Radio}</h4>
+    <h4>  USB : ${phone.USB}</h4>
+    <h4> WLAN  : ${phone.WLAN}</h4>
+   
     </div>
     `
     phoneDatails.appendChild(div);
 }
+// relesdate
+const relesButton = slug => {
+    const url = `
+    https://openapi.programming-hero.com/api/phone/${slug}
+    `
+    fetch(url)
+        .then(res => res.json())
+        .then(phones => displayRelesDate(phones.data.releaseDate))
+}
+const displayRelesDate = phone => {
+    const phoneDatails = document.getElementById('phone-details')
+    const div = document.createElement('div')
+    div.classList.add('col');
+    div.innerHTML = `  
+    <div class="card-body">
+    <h1>  ReleaseDate </h1>
+    <h4> ReleaseDate : ${phone ? phone : "Releasdate not found"}</h4>
+    </div>
+    `
+    phoneDatails.appendChild(div);
 
+}
